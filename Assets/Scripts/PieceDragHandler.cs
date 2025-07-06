bc53bc6d0e43e3bd8725315c9dfeb8346fd99838
adag21044 <adag21044@gmail.com>
2025-07-06 11:30:03 +0300

    detection fixed

--- Assets/Scripts/PieceDragHandler.cs
+++ Assets/Scripts/PieceDragHandler.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PieceDragHandler : MonoBehaviour, IPointerDownHandler,IPointerUpHandler, IDragHandler
+public class PieceDragHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     private Vector3 _offset;
+    [SerializeField] private LayerMask layerMask;
+    private Vector3 homePosition;
+
+    private void Start()
+    {
+        homePosition = transform.position; // Store the initial position as home
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("Down");
@@ -18,13 +26,42 @@ public class PieceDragHandler : MonoBehaviour, IPointerDownHandler,IPointerUpHan
 
         var target = Camera.main.ScreenToWorldPoint(eventData.position);
         target += _offset;
-        target.z = 0; 
+        target.z = 0;
         transform.position = target;
     }
 
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("PieceDragHandler  : Drag ended for: " + gameObject.name);
+        var hit = Hit();
+
+        if (hit)
+        {
+            var target = hit.transform.position;
+            target.z = 0; // Ensure z position is zero
+            transform.position = target; // Snap to the hit position
+        }
+        else
+        {
+            BackHome();
+        }
+    }
+
+    private void BackHome()
+    {
+        Debug.Log("Back Home");
+        transform.position = homePosition; // Reset to the initial position
+    }
+
+    private RaycastHit2D Hit()
+    {
+        var origin = transform.position;
+        return Physics2D.Raycast(origin, Vector3.forward, 10f, layerMask);
+    }
+
+    private void FixedUpdate()
+    {
+        var hit = Hit();
+        Debug.Log(hit ? $"Hit: {hit.transform.name}" : "No Hit");
     }
 }
