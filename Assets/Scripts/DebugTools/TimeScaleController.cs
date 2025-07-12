d74229e537371ba83957a934428d1990e5e0e60e
adag21044 <adag21044@gmail.com>
2025-07-12 21:11:27 +0300

    time freeze toggle added

---
+++ Assets/Scripts/DebugTools/TimeScaleController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class TimeScaleController : MonoBehaviour
+{
+    public static TimeScaleController Instance;
+
+    [Range(0.1f, 5f)]
+    [SerializeField] private float timeScale = 1f;
+    private bool isTimeFreeze = false;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject); // Seviye geçişinde yok olmasın
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Minus)) // Zamanı yavaşlat
+        {
+            SetTimeScale(timeScale - 0.5f);
+        }
+        else if (Input.GetKeyDown(KeyCode.Plus)) // Zamanı hızlandır
+        {
+            SetTimeScale(timeScale + 0.5f);
+        }
+        else if (!isTimeFreeze && Input.GetKeyDown(KeyCode.I)) // Zamanı sıfırla (dondur)
+        {
+            SetTimeScale(0f);
+            isTimeFreeze = true;
+        }
+        else if (isTimeFreeze && Input.GetKeyDown(KeyCode.I)) // Zamanı eski haline getir
+        {
+            SetTimeScale(1f);
+            isTimeFreeze = false;
+        }
+    }
+
+    public void SetTimeScale(float scale)
+    {
+        timeScale = Mathf.Clamp(scale, 0f, 5f);
+        Time.timeScale = timeScale;
+        Debug.Log($"⏱️ Time scale set to {timeScale}");
+    }
+
+    public float GetTimeScale() => timeScale;
+}
